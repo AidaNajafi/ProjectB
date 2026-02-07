84ff1a3d4dd4f422a3be1a3de096b4d4f6474c54
aida.najafi <aida.najafi@pintapin.local>
2026-02-07 04:56:33 +0330

    fixed app bug

--- internal/app/app.go
+++ internal/app/app.go
@@ -12,12 +12,12 @@ import (
 func App(flags []string) error {
 	cfg, err := config.LoadConfig(flags)
 	if err != nil {
-		return fmt.Errorf("loadConfig failed", err)
+		return fmt.Errorf("loadConfig failed: %w", err)
 	}
 	repo := repository.NewCSVRepo(cfg.FilePath)
 	svc := service.NewAuthService(repo)
 	addr := fmt.Sprintf(":%d", cfg.Port)
-	log.Printf("Starting server on port: %d", addr)
+	log.Printf("Starting server on port: %s", addr)
 	router := server.SetUpRoutes(svc)
 	return router.Run(addr)
 
